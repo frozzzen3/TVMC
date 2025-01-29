d41600eaf5de9bceaed91dfc81debfa688a17e92
Guodong Chen <chen.guod@northeastern.edu>
2025-01-29 17:59:29 -0500

    update

--- TVMC/README.md
+++ TVMC/README.md
@@ -1,29 +1,62 @@
 # **TVMC: Time-Varying Mesh Compression Using Volume-Tracked Reference Meshes**
 
+Guodong Chen, Filip Hácha, Libor Váša, Mallesham Dasari
+
+![alt text](https://github.com/frozzzen3/TVMC/blob/main/images/TVMC-workflow.png?raw=true)
+
+This repository contains the official authors implementation associated with the paper **"TVMC: Time-Varying Mesh Compression Using Volume-Tracked Reference Meshes"**, which can be found [here] (to be added), accepted by [2025 ACM MMSys](https://2025.acmmmsys.org/).
+
+## Step-by-step Tutorial
+
 I'll use `Dancer` dataset from [TDMD](https://multimedia.tencent.com/resources/tdmd) to show the whole pipeline.
 
-Step 1: As-rigid-as-possible Volume tracking
+## Step 1: As-rigid-as-possible Volume tracking
 
 go to the root `cd ./arap-volume-tracking `
 
-build `dotnet build -c release`, The build application is in `./bin` folder.
+build 
 
-`dotnet ./bin/client.dll ./config/max/config-dancer-max.xml`
+``` 
+dotnet build -c release
+```
+
+The build application is in `./bin` folder.
+
+```
+dotnet ./bin/client.dll ./config/max/config-dancer-max.xml
+```
 
 Now, you can find volume tracking results in the `<outDir>` folder.
 
 `.xyz` files represent the coordinates of each centers 
 
-`.xyz` files represent the coordinates of each centers 
+`.txt` files represent the transformation for each centers between frames 
 
-Time-varying mesh sequences are stored in `./arap-volume-tracking/data`
 
-configuration files can be found in `./arap-volume-tracking/config`
+
+Some other time-varying mesh sequences are stored in `./arap-volume-tracking/data`.
+
+Configuration files can be found in `./arap-volume-tracking/config`
 
 You may be curious about why there are three folders, actually `./iir` `./impr` `./max` represent different configuration files for three tracking modes, please find more details and examples [here](https://github.com/frozzzen3/TVMC/blob/main/arap-volume-tracking/README.md).
 
-After setting configurations, run
+To run ARAP volume tracking on your own dataset,  create configuration file then execute
 
-`dotnet ./bin/client.dll <config_file.xml>` 
+```
+dotnet ./bin/client.dll <config_file.xml> 
+```
 
-(e.g., `dotnet ./bin/client.dll ./config/max/config-dancer-max.xml`)
+
+
+Global optimization mode is optional, it can only be adopted after getting volume centers, it will try to remove a certain number of abnormal volume centers and adjust positions of the remains.
+
+
+
+## Step 2: Using multi-dimensional scaling to generate reference centers
+
+go to the root folder of TVMC, `cd ./TVMC`, and run
+
+```
+python .\get_reference_center.py --dataset dancer --num_frames 10 --num_centers 2000 --centers_dir ..\arap-volume-tracking\data\dancer_2000
+```
+
---
+++ TVMC/get_reference_center.py
@@ -0,0 +1,86 @@
+import argparse
+import os
+import numpy as np
+import open3d as o3d
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+
+# Command-line argument parser
+parser = argparse.ArgumentParser(description="Process 3D data and perform MDS.")
+parser.add_argument('--dataset', type=str, required=True, help="Dataset name (e.g., 'Dancer')")
+parser.add_argument('--num_frames', type=int, required=True, help="Number of frames to process")
+parser.add_argument('--num_centers', type=int, required=True, help="Number of volume centers (pointCount)")
+parser.add_argument('--centers_dir', type=str, required=True, help="Path for the volume centers")
+parser.add_argument('--file_extension', type=str, default=".xyz", help="File extension for the input files")
+
+args = parser.parse_args()
+
+# Extract arguments
+dataset = args.dataset
+num_frames = args.num_frames
+num_centers = args.num_centers
+centers_dir = args.centers_dir
+file_extension = args.file_extension
+
+print("open3d version:", o3d.__version__)
+print(f"Dataset: {dataset}, Frames: {num_frames}, Centers: {num_centers}")
+
+xyz_files = [f for f in os.listdir(centers_dir) if f.endswith('.xyz')]
+max_distance_matrix = np.zeros((num_centers, num_centers))
+
+for xyz_file in xyz_files:
+    print("Loading and processing: ", xyz_file)
+    all_points = []
+    filename = os.path.join(centers_dir, xyz_file)
+    with open(filename, 'r') as file:
+        for line in file:
+            points = list(map(float, line.split()))
+            all_points.append(points)
+        all_points = np.array(all_points)
+        #print(all_points[0])
+        for i in range(num_centers):
+            for j in range(i + 1, num_centers):
+                if np.max(np.linalg.norm(all_points[i] - all_points[j], axis=0)) > max_distance_matrix[i, j]:
+                    max_distance_matrix[i, j] = np.max(np.linalg.norm(all_points[i] - all_points[j], axis=0))
+
+                max_distance_matrix[j, i] = max_distance_matrix[i, j]
+
+output_file = f"{centers_dir}/{dataset}_distance_matrix_{num_centers}.txt"
+np.savetxt(output_file, max_distance_matrix)
+
+print("Distance Matrix generated and saved!")
+print("Find Distance Matrix here: ", output_file)
+#print(max_distance_matrix, max_distance_matrix.shape)
+
+random_state = np.random.randint(0, 100000)
+print(f"Feed Distance Matrix to multi-dimensional scaling to get reference centers, random_state = {random_state}")
+mds = MDS(n_components=3, metric=True, dissimilarity='precomputed', n_jobs=-1, eps=1e-20, verbose=0, random_state=random_state,
+          n_init=6, max_iter=300)
+reference_centers = mds.fit_transform(max_distance_matrix)
+
+center_datas = []
+for xyz_file in xyz_files:
+    center_filename = os.path.join(centers_dir, xyz_file)
+    center_data = np.loadtxt(center_filename)
+    center_datas.append(center_data)
+
+centers = center_datas[0]
+
+print("Singular Value Decomposition...")
+centers_mean = np.mean(centers, axis=0)
+reference_centers_mean = np.mean(reference_centers, axis=0)
+centers_centered = centers - centers_mean
+reference_centers_centered = reference_centers - reference_centers_mean
+
+cov_matrix = np.dot(centers_centered.T, reference_centers_centered)
+U, _, Vt = np.linalg.svd(cov_matrix)
+R = np.dot(U, Vt)
+
+reference_centers_aligned = np.dot(reference_centers_centered, R.T)
+reference_centers_aligned = reference_centers_aligned + centers_mean
+
+output_filename = f"{centers_dir}/reference_centers_aligned.xyz"
+np.savetxt(output_filename, reference_centers_aligned, fmt='%f', delimiter=' ')
+
+print("Reference centers saved!")
+print("Find reference centers here: ", output_filename)
